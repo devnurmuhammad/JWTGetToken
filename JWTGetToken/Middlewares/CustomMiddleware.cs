8b5aea81322c8930fd004d2f9f6aa2e31b3cb7ee
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-15 15:54:37 +0500

    Niddlewares added

---
+++ JWTGetToken/Middlewares/CustomMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace JWTGetToken.CustomMiddlewares
+{
+    public class CustomMiddleware
+    {
+        private RequestDelegate _requestDelegate;
+
+        public CustomMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+        public void InvokeAsync(HttpContext context)
+        {
+            var builder = WebApplication.CreateBuilder();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("V1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "AuthDemo",
+                    Description = "Auth Demo Description"
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Description = "Bearer Authentication",
+                    Type = SecuritySchemeType.Http
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement(){
+                {
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                        new List<string> ()
+                    }
+                });
+            });
+        }
+    }
+}
